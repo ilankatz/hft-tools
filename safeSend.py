653ac875c6f922e2a187cf899f6750b4abbecdfa
ilankatz <ilankatz100@gmail.com>
2022-12-18 14:31:12 -0500

    Create safeSend.py
    Send ether and ERC20 tokens with encrypted pieces of private keys so no individual ever has access to any part of a private key

---
+++ safeSend.py
@@ -0,0 +1,66 @@
+from web3 import Web3
+from dotenv import load_dotenv
+import os
+from keySign import encrypt, sign, updateenv
+load_dotenv()
+
+
+def send(fromKeys, fromAddress, toAddress, amount):
+    nonce = web3.eth.getTransactionCount(fromAddress)
+    estimate = web3.eth.estimateGas({'to': toAddress, 'from': fromAddress, 'value': amount})
+    tx = {
+        'nonce': nonce,
+        'to': toAddress,
+        'value': amount,
+        'gas': estimate,
+        'gasPrice': web3.eth.gas_price
+    }
+    signed_tx = web3.eth.account.sign_transaction(tx, fromKeys)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    return(web3.toHex(tx_hash))
+
+def encSend(slices, fromAddress, toAddress, amount, envvars, incGas = False):
+    key = sign(slices)
+    slices = encrypt(key)
+    updateenv(slices,envvars)
+
+    if incGas == True:
+        amount -= (web3.eth.estimateGas({'to': toAddress, 'from': fromAddress, 'value': amount}))*web3.eth.gas_price
+    ret = send(key, fromAddress, toAddress, amount)
+    return ret
+
+def senderc20(fromKey, address, toAddress, fromAddress, amount, chainid):
+    abi = [
+        {
+        "constant": False,
+        "inputs": [
+            {"name": "to", "type": "address"},
+            {"name": "value", "type": "uint256"},
+        ],
+        "name": "transfer",
+        "outputs": [{"name": "success", "type": "bool"}],
+        "payable": False,
+        "stateMutability": "nonpayable",
+        "type": "function",
+        }
+    ]
+    contract = web3.eth.contract(address=address,abi=abi)
+    tx = contract.functions.transfer(
+        toAddress,
+        amount,
+    ).buildTransaction({
+        'chainId': chainid,
+        'gas': 70000,
+        'gasPrice': web3.toWei('1', 'gwei'),
+        'nonce': web3.eth.getTransactionCount(fromAddress),
+    })
+    signed_tx = web3.eth.account.signTransaction(tx, private_key=fromKey)
+    hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    return(web3.toHex(hash))
+
+def safeSendErc20(slices, tokenAddress, toAddress,fromAddress,amount,chainid, envvars):
+    key = sign(slices)
+    ss = encrypt(key)
+    updateenv(ss,envvars)
+    ret = senderc20(key,tokenAddress,toAddress,fromAddress,amount,chainid)
+    return ret
