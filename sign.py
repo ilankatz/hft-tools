c7e7dfa6ae6eecb8e4f95f061f3017f1f0241233
ilankatz <ilankatz100@gmail.com>
2022-12-17 22:54:23 -0500

    Rewrite sign.py
    Cleaned it up, everything works now, gonna add some more stuff to make it more secure.  I also want to get a way to store encrypted pieces on different devices and safely send them to a central server only connected to web3 to keep everything secure.

--- sign.py
+++ sign.py
@@ -4,50 +4,25 @@ import os
 load_dotenv()
 
 def sign(slice1,slice2,slice3,slice4):
-    crypt1 = slice1[1:121]
-    dec1 = slice1[121:167]
-    crypt2 = slice2[1:121]
-    dec2 = slice2[121:167]
-    crypt3 = slice3[1:121]
-    dec3 = slice3[121:167]
-    crypt4 = slice4[1:121]
-    dec4 = slice4[121:167]
-    decrypt1 = Fernet(dec4)
-    slice1 = decrypt1.decrypt(crypt1).decode()
-    decrypt2 = Fernet(dec1)
-    slice2 = decrypt2.decrypt(crypt2).decode()
-    decrypt3 = Fernet(dec2)
-    slice3 = decrypt3.decrypt(crypt3).decode()
-    decrypt4 = Fernet(dec3)
-    slice4 = decrypt4.decrypt(crypt4).decode()
-    slices = [slice2, slice1, slice3, slice4]
+    pieces = [slice1, slice2, slice3, slice4]
+    pieces.sort()
+    crypts = []
+    decs = []
+    for piece in pieces:
+        crypts.append(piece[1:121])
+        decs.append(piece[121:167])
+    fernets = []
+    for dec in decs:
+        fernets.append(Fernet(dec))
+    order = [3,0,1,2]
+    fernets[:] = [fernets[i] for i in order]
+    slices = []
+
+    for i in range(len(fernets)):
+        cur = fernets[i].decrypt(crypts[i]).decode()
+        slices.append(cur)
     slices.sort()
     fullKey = ''
     for slice in slices:
         fullKey = fullKey + slice[1:]
     return fullKey
-
-key = os.getenv('key1')
-
-slice1 = key[0:16]
-slice2 = key[16:32]
-slice3 = key[32:48]
-slice4 = key[48:64]
-
-
-slices = slice1 + slice2 + slice3 + slice4
-slice1 = '1' + slice1
-slice2 = '2' + slice2
-slice3 = '3' + slice3
-slice4 = '4' + slice4
-fernet1 = Fernet(os.getenv('fernetKey1'))
-fernet2 = Fernet(os.getenv('fernetKey2'))
-fernet3 = Fernet(os.getenv('fernetKey3'))
-fernet4 = Fernet(os.getenv('fernetKey4'))
-
-enc1 = '2' + fernet1.encrypt(slice1.encode()).decode('utf-8') + os.getenv('fernetKey2')
-enc2 = '3' + fernet2.encrypt(slice2.encode()).decode('utf-8') + os.getenv('fernetKey3')
-enc3 = '4' + fernet3.encrypt(slice3.encode()).decode('utf-8') + os.getenv('fernetKey4')
-enc4 = '1' + fernet4.encrypt(slice4.encode()).decode('utf-8') + os.getenv('fernetKey1')
-
-print(sign(enc1,enc2,enc3,enc4) == os.getenv('key1'))
