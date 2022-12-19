21212fabd30fc5acd9d8479f28630fcdade1dbdc
ilankatz <ilankatz100@gmail.com>
2022-12-18 20:32:46 -0500

    Clean up createAccount.py
    Shortened up the code a bit, I'll consider this done

--- createAccount.py
+++ createAccount.py
@@ -1,11 +1,7 @@
-from web3 import Web3
 from eth_account import Account
 import secrets
-provider = 'YOUR INFURA KEY'
-web3 = Web3(Web3.HTTPProvider(provider))
 
 priv = secrets.token_hex(32)
-private_key = "0x" + priv
-print ("SAVE BUT DO NOT SHARE THIS:", private_key)
-acct = Account.from_key(private_key)
+print ("SAVE BUT DO NOT SHARE THIS:", priv)
+acct = Account.from_key("0x" + priv)
 print("Address:", acct.address)
